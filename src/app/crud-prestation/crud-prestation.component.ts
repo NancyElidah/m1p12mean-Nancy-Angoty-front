da2e065964a325da913abe05b20f1d5def382554
Angoty <angotyrabarijaona@gmail.com>
2025-03-30 21:43:44 +0300

    changing crud-prestation.component
    changement de path

--- src/app/crud-prestation/crud-prestation.component.ts
+++ src/app/crud-prestation/crud-prestation.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import {FormBuilder, FormGroup, Validators,ReactiveFormsModule} from '@angular/forms';
-import { PrestationService } from '../services/prestation.service';
-import { SnackbarService } from '../services/snack-bar.service';
-import { Propos } from '../models/Propos';
+import { PrestationService } from '../service/prestation.service';
+import { SnackbarService } from '../service/snack-bar.service';
+import { Prestation } from '../models/Prestation';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { MessageModule } from 'primeng/message';
@@ -20,9 +20,9 @@ import { InputTextModule } from 'primeng/inputtext';
 
 })
 export class CrudPrestationComponent {
-  formPropos!: FormGroup;  
-  propos: Propos = new Propos();
-  items: Propos[] = []; 
+  formPrestation!: FormGroup;  
+  propos: Prestation = new Prestation();
+  items: Prestation[] = []; 
   constructor(
     private fb: FormBuilder,
     private prestationService: PrestationService,
@@ -34,11 +34,53 @@ export class CrudPrestationComponent {
     this.visible = true; 
   }
   ngOnInit(): void {
-    this.formPropos = this.fb.group({
+    this.formPrestation = this.fb.group({
       intitule: ['', Validators.required],  
+      prix: ['', Validators.required]
     });
-  } 
+    this.getAllPropos();
+  }
+  
+  getAllPropos() {
+    this.prestationService.getPrestation().subscribe(
+      (data) => {this.items = data;  },
+      (error) => {
+        console.error('Erreur lors de la récupération des propos:', error);
+        this.snackbarService.open(
+          'Erreur lors de la récupération des données.',
+          'Récupération Prestation',
+          'error'
+        );
+      }
+    );
+  }
+  onSubmit() {
+    if (this.formPrestation.valid) {
+      this.propos = this.formPrestation.value;
+      this.prestationService.addPrestation(this.propos).subscribe(
+        (response) => {
+          this.snackbarService.open(
+            'Prestation enregistré avec succès!',
+            'Enregistrement Prestation',
+            'success'
+          );
+          this.resetForm(); 
+          this.getAllPropos();
+          this.router.navigate(['/prestation']);  
+        },
+        (error) => {
+          console.error('Erreur lors de l’enregistrement :', error);
+          this.snackbarService.open(
+            'Une erreur est survenue. Veuillez réessayer.',
+            'Enregistrement Prestation',
+            'error'
+          );
+        }
+      );
+    }
+  }
+
   resetForm() {
-    this.formPropos.reset(); 
+    this.formPrestation.reset(); 
   }
 }
