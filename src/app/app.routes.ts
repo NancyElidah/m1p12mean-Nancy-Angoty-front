a6a657de5555308da42735924ada1020689b21eb
NancyElidah <nancyandriamahanintsoa@gmail.com>
2025-04-01 10:15:19 +0200

    DEBUG CONFLICT

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,16 @@
 import { Routes } from '@angular/router';
 import { HeaderClientComponent } from './header-client/header-client.component';
 import { LoginClientComponent } from './login-client/login-client.component';
+import { UtilisateurCrudClientComponent } from './utilisateur-crud-client/utilisateur-crud-client.component';
+import { LoginComponent } from './mec-manager/login/login.component';
+import { AccueilComponent } from './mec-manager/header/accueil/accueil.component';
+import { InscriptionComponent } from './mec-manager/mecanicien/inscription/inscription.component';
 
 export const routes: Routes = [
   { path: '', component: HeaderClientComponent },
   { path: 'login-client', component: LoginClientComponent },
+  { path: 'inscription-client', component: UtilisateurCrudClientComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'accueil', component: AccueilComponent },
+  { path: 'inscription-mec', component: InscriptionComponent },
 ];
---
+++ src/app/models/User.ts
@@ -0,0 +1,9 @@
+export class User {
+  id?: String;
+  nom!: String;
+  prenom!: String;
+  email!: String;
+  password!: String;
+  statut!: Number;
+  telephone?: String;
+}
